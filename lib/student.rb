edd4e4716700892140f9da5e8dea6606315eedcc
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-05 19:47:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,13 @@ def save
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-end 
+end
+
+def self.create(name:, album:)
+  student = Song.new(name, album)
+  song.save
+  song
+end
 
 end
 
