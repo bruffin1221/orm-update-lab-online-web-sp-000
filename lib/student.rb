a3e80ae820ea1dbc6c7a322c0c01bea9e23b4d29
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-05 21:44:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,12 +50,11 @@ def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
-      LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
 end
 
