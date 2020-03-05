e078c715ca28d8b10b9780e5d7dc04a0755a85da
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-05 21:25:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,11 @@ def self.create(name, grade)
   student_new
 end
 
+def self.new_from_db
+  student=self.new
+  new_student.id=row[0]
+  new_student.name=row[1]
+  new_student.grade=row[2]
 end
 
 
